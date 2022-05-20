692f5a00cfa591a1b2690b5843ac29afbc88d11c
Adriano Andrade <webmail.adriano@gmail.com>
2022-05-19 23:00:28 -0300

    corrigindo dockerfile node 2

--- node/index.js
+++ node/index.js
@@ -13,19 +13,22 @@ const connection = mysql.createConnection(config)
 
 const sql = `INSERT INTO people(name) values('Adriano')`
 connection.query(sql)
-
+connection.end()
 
 
 app.get('/', (req,res) => {
-  connection.query("SELECT * FROM people", function (err, result, fields) {
+  const mysql = require('mysql')
+  const connection = mysql.createConnection(config)
+  connection.query("SELECT * FROM people", function (err, result, fields, resultHtml) {
     if (err) throw err;
     resultHtml = '<table><tr><td>id</td><td>Nome</td></tr>';
     for (var i=0, len=result.length; i<len; i++){
       resultHtml += '<tr><td>'+result[i]['id']+'</td><td>'+result[i]['name']+'</td></tr>';
     }
     resultHtml += '</table>';
+    res.send('<h1>Full Cycle</h1><br />' + resultHtml);
   });
-  res.send('<h1>Full Cycle</h1><br />' + resultHtml);
+  connection.end()
 })
 // connection.end()
 app.listen(port, ()=> {
